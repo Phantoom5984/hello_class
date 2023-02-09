000b90f13a288685730e06373b93031c38a5156e
Sergio Salvago Medina <salvagomedinasergio@gmail.com>
2023-02-09 18:24:27 +0100

    sergio

--- hello.py
+++ hello.py
@@ -3,9 +3,12 @@ def antonio():
 
 def samuel():
     print("Hola soy Samuel, compañero de DAW")
+    def sergio():
+    print("Hola soy Sergio, compañero de DAW")
 
 if __name__ == "__main__":
     print("Say hello to class ")
     antonio()
     samuel()
+    sergio()
     
